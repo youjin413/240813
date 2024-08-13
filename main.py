25c3cdbe6a415ad2875a33beab3d2891a4b5ec44
youjin413 <111631885+youjin413@users.noreply.github.com>
2024-08-13 15:53:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import settings
 #load_dotenv()
 
 # python -m streamlit run main.py
-st.title("📝석리송 전용 GPT(테디노트 이경록 대표님 코드 기반)")
+st.title("📝유징's GPT(테디노트 이경록 대표님 코드, 석리송님 코드 기반)")
 
 config = settings.load_config()
 if "api_key" in config:
